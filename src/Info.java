8bec4505fef1375b9f75ff0e2918ad3baaf32daf
Shamak Patel <shamak@dhcp-172-29-17-10.guest.uc.edu>
2023-02-03 12:03:49 -0500

    Initial commit

---
+++ src/Info.java
@@ -0,0 +1,19 @@
+public class Info {
+    public static void main(String[] args) {
+        int kids = 2;
+        int priceGas = 3;
+        int favNum = 10;
+        int size = 10;
+        String month = "January";
+        String name ="Shamak Patel";
+        String rain = "yes";
+
+    System.out.println( "the number of kids in your family: "+ kids);
+    System.out.println(" the answer to is it raining? : " + rain);
+    System.out.println (" the price of a gallon of gas : " + priceGas);
+    System.out.println( "your favorite number : " + favNum);
+    System.out.println( "your shoe size: " + size);
+    System.out.println( "your birth month: " + month);
+    System.out.println( "your full name: " + name);
+    }
+}
